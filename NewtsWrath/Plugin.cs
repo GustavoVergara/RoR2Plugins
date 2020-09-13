91940bce8eb7e9150639da723359fd4bb85b2c7c
Gustavo Vergara <gustavovgarcia.sl@gmail.com>
2020-09-13 19:32:18 -0300

    Move Utils to its own file

--- NewtsWrath/Plugin.cs
+++ NewtsWrath/Plugin.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using BepInEx;
+﻿using BepInEx;
 using RoR2;
 using System.Collections.Generic;
 using UnityEngine;
@@ -55,20 +54,6 @@ namespace NewtsWrath
             }
         }
 
-    }
-
-    public static class Utils
-    {
-        public static Texture2D LoadTexture2D(Byte[] resourceBytes)
-        {
-            //Check to make sure that the byte array supplied is not null, and throw an appropriate exception if they are.
-            if (resourceBytes == null) throw new ArgumentNullException(nameof(resourceBytes));
-
-            //Create a temporary texture, then load the texture onto it.
-            var tempTex = new Texture2D(128, 128, TextureFormat.RGBAFloat, false);
-            ImageConversion.LoadImage(tempTex, resourceBytes, false);
-            return tempTex;
-        }
     }
 
 }
---
+++ NewtsWrath/Utils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace NewtsWrath
+{
+    public static class Utils
+    {
+        public static Texture2D LoadTexture2D(Byte[] resourceBytes)
+        {
+            //Check to make sure that the byte array supplied is not null, and throw an appropriate exception if they are.
+            if (resourceBytes == null) throw new ArgumentNullException(nameof(resourceBytes));
+
+            //Create a temporary texture, then load the texture onto it.
+            var tempTex = new Texture2D(128, 128, TextureFormat.RGBAFloat, false);
+            ImageConversion.LoadImage(tempTex, resourceBytes, false);
+            return tempTex;
+        }
+    }
+
+}
